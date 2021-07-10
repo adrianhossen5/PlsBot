628ec191436e063eaabf7e7371e64bd4678d91e0
adrianhossen5 <86932142+adrianhossen5@users.noreply.github.com>
2021-07-10 19:35:32 -0400

    Update flowerBot.js

--- flowerBot.js
+++ flowerBot.js
@@ -17,11 +17,21 @@ function readyDiscord(){
 client.on('message', gotMessage);
 async function gotMessage(msg){
 
+    let tokens = msg.content.split(' ');
+
     console.log(msg.content);
 
-    if (msg.content === '!pls flower'){
+    let keywords = 'Your mom';
 
-        let url = `https://g.tenor.com/v1/search?q=flower&key=${process.env.TENORKEY}&limit=8`
+    if (tokens[0] === '!pls'){
+
+        if(tokens.length > 1){
+
+            keywords = tokens.slice(1, tokens.length).join(" ");
+
+        }
+
+        let url = `https://g.tenor.com/v1/search?q=${keywords}&key=${process.env.TENORKEY}`
         let response = await fetch(url);
         let json = await response.json();
         const index = Math.floor(Math.random() * json.results.length);
