335a22212a68870da86cafac39749ecc4710752a
adrianhossen5 <86932142+adrianhossen5@users.noreply.github.com>
2021-07-17 11:04:38 -0400

    Update and rename PlsBot.js to plsBot.js

--- plsBot.js
+++ plsBot.js
@@ -8,7 +8,6 @@ client.on('ready', readyDiscord);
 client.on('message', gotMessage);
 
 console.log('Hello There');
-
 function readyDiscord(){
 
     console.log('Hello!');
@@ -21,7 +20,6 @@ async function gotMessage(msg){
     console.log(msg.content);
     let keywords = 'keywords';
     if (tokens[0] === '!pls'){
-
         if(tokens.length > 1){
 
             keywords = tokens.slice(1, tokens.length).join(" ");
@@ -31,6 +29,7 @@ async function gotMessage(msg){
         const url = `https://g.tenor.com/v1/search?q=${keywords}&key=${process.env.TENORKEY}`
         const response = await fetch(url);
         const json = await response.json();
+
         const index = Math.floor(Math.random() * json.results.length);
         msg.reply(json.results[index].url)
     
